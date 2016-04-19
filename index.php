0d366c4f9dd04c1b8fd96711a0c0a23f2146298c
Nico Greco <ncgreco1440@gmail.com>
2016-04-19 19:34:41 -0400

    html

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
-<?php
-// Just a single include statement!
-include "lib/autoload.php";
-use Database;
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Tech - Nico</title>
+    </head>
+    <body>
+        <?php
+            // Just a single include statement!
+            include "lib/autoload.php";
+            use Database;
 
-echo "Hello, World!<br/><br/>";
+            echo "Hello, World!<br/><br/>";
 
-$web = new Website("Leverage");
-$webpage = new Webpage("A New Web Page");
-Database\Connection::connect("localhost", "DB_LEVERAGE", "client", "client");
-Database\Extra::invoked();
+            $web = new Website("Leverage");
+            $webpage = new Webpage("A New Web Page");
+            Database\Connection::connect("localhost", "DB_LEVERAGE", "client", "client");
+            Database\Extra::invoked();
+        ?>
+    </body>
+</html>
 
