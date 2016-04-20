0d4133c320808badf6e67307fff4612546ab5717
Nico Greco <ncgreco1440@gmail.com>
2016-04-20 09:05:57 -0400

    database connection

--- lib/classes/connection.class.php
+++ lib/classes/connection.class.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 namespace Database;
 
 use Application\Env;
 Env\Env::createEnviornment();
 /** ====================================================================================
 
-    1. 
+    1.
 
-    Namespaces are extremely useful when you have a bunch of classes that 
-    that relate to one another in some way, but don't necessarily 
+    Namespaces are extremely useful when you have a bunch of classes that
+    that relate to one another in some way, but don't necessarily
     inherit from one another. It's extremely useful for cutting
     done on amount of files and folders you need.
 
@@ -17,11 +17,12 @@ class Connection
 {
     private static $_connection = false;
 
-    public static function connect() 
+    public static function connect()
     {
         $db = Env\Env::fetchEnv();
-        if(!$_connection = new \mysqli($db['DB_HOST'], $db['DB_USERNAME'], $db['DB_PASSWORD'], $db['DB_DATABASE']))
-            echo "Connection Error!<br/>";
+        $_connection = new \mysqli($db['DB_HOST'], $db['DB_USERNAME'], $db['DB_PASSWORD'], $db['DB_DATABASE'])
+        if($_connection->connect_erno > 0)
+            echo "Connection Error! ". $_connection->connect_erno ."<br/>";
         else
             echo "Database Obtained A Successful Connection!<br/>";
     }
